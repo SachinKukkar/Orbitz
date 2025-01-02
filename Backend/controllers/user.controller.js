5cbae6d17862a0c1bb41ebf6e68ab0c4ee5eb995
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-02 19:47:01 +0530

    done till 31:23

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require("express-validator");
+
+
+module.exports.registerUser = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { firstname, lastname, email, password } = req.body;
+
+    const hashedPassword = await userModel.hashedPassword(password);   // ham hashed password pass krenge naa ki as a plain text
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
+
+}
