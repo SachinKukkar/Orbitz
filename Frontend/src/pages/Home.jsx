c3dad19ed0e4f9a94d1da43563d097f15ed359a1
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-07 23:42:27 +0530

    3:00:00

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-4 flex justify-between flex-col w-full'>
                 <img className='w-20 ml-5 mt-2' src="./src/assets/logo4copy.png" alt="Orbitz" />
                 <div className='bg-white pb-7 py-4 px-4'>
-                    <h2 className='text-3xl font-bold'>Get started with Orbitz</h2>
+                    <h2 className='text-2xl font-bold'>Get started with Orbitz</h2>
                     <Link to="/login" className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
                 </div>
             </div>
