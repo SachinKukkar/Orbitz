f9fd1a95a067154eca65876632f2d3e09627081f
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-09 00:08:48 +0530

    3:23:53

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserDataContext } from '../context/userContext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const UserLogin = () => {
   const [email, setemail] = useState('')
   const [password, setpassword] = useState('')
   const [userData, setuserData] = useState({})
 
-  const submitHandler = (e) => {
+  const {user,setuser} = useContext(UserDataContext)
+  const navigate = useNavigate()
+
+  const submitHandler = async(e) => {
     e.preventDefault();
-    setuserData({
+    
+    const userData = {
       email: email,
       password: password
-    })
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, userData)
+
+    if(response.status === 200){
+      const data = response.data
+      setuser(data.user)
+      localStorage.setItem('token',data.token)
+      navigate('/home')
+    }
+
     setemail('')
     setpassword('')
   }
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { useState,useContext,createContext } from 'react'
+import axios from 'axios'
+import { UserDataContext } from '../context/userContext'
+
 
 const UserSignup = () => {
   const [email, setEmail] = useState('')
@@ -9,17 +12,30 @@ const UserSignup = () => {
   const [lastName, setLastName] = useState('')
   const [userData, setUserData] = useState({})
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate()
+  const {user,setuser} = useContext(UserDataContext)
+
+  const submitHandler = async(e) => {
     e.preventDefault();
 
-    setUserData({
-      fullName: {
-        firstName: firstName,
-        lastName: lastName
+    const newUser = {
+      fullname:{
+        firstname: firstName,
+        lastname: lastName
       },
       email: email,
       password: password
-    })
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
+
+    if(response.status === 201){
+      const data = response.data
+      
+      setuser(data.user)
+      localStorage.setItem('token',data.token)
+      navigate('/home') 
+    }
 
     setEmail('')
     setFirstName('')
@@ -82,7 +98,7 @@ const UserSignup = () => {
           />
           <button
             className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full'>
-            Signup
+            Create Account
           </button>
 
         </form>
