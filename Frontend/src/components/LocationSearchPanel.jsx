d1c7008c9292d45e94594ac77d9ef73dd8ef8a2a
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-10 00:27:42 +0530

    4:37:38

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* This is just a sample data */}
+
+    <div className='flex gap-4 items-center my-2 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+        <h4 className='font-medium'>24A, Near Chawla's Cafe, Xavier's Coding Classes, Bangalore</h4>
+    </div>
+
+    <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+        <h4 className='font-medium'>24A, Near Chawla's Cafe, Xavier's Coding Classes, Bangalore</h4>
+    </div>
+
+    <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+        <h4 className='font-medium'>24A, Near Chawla's Cafe, Xavier's Coding Classes, Bangalore</h4>
+    </div>
+
+    <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+        <h4 className='font-medium'>24A, Near Chawla's Cafe, Xavier's Coding Classes, Bangalore</h4>
+    </div>
+
+    <div className='flex gap-4 items-center my-5 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+        <h4 className='font-medium'>24A, Near Chawla's Cafe, Xavier's Coding Classes, Bangalore</h4>
+    </div>
+
+    
+
+    </div>
+  )
+}
+
+export default LocationSearchPanel
