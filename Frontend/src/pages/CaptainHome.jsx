b9ea9ade4c3cf07eae61c33b23ccf7b74085f01c
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-20 23:49:13 +0530

    6:36:05

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
 
 const CaptainHome = () => {
   return (
@@ -19,38 +21,13 @@ const CaptainHome = () => {
       </div>
 
       <div className='h-2/5 p-6'>
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center justify-start gap-3'> 
-            <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToIV84qK7oSHGMk_7wiZjH3K__lvmkJr9hjw&s" alt="" />
-            <h4 className='text-lg font-medium'>Harsh Patel </h4>
-          </div>
-          <div >
-            <h4 className='text-xl font-semibold'>₹295.47</h4>
-            <p className='text-sm text-gray-600'>Earned</p>
-          </div>
-        </div>
-        <div className='flex p-3 mt-8 bg-gray-100 rounded-xl justify-center gap-6 items-start'>
-
-          <div className='text-center'>
-          <i className='text-3xl mb-2 font-thin ri-timer-2-line'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-500'>Hours Online</p>
-          </div>
-          
-          <div className='text-center'>
-          <i className='text-3xl mb-2 font-thin ri-speed-up-line'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-500'>Hours Online</p>
-          </div>
-         
-          <div className='text-center'>
-          <i className='text-3xl mb-2 font-thin ri-booklet-line'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-500'>Hours Online</p>
-          </div>
-
-        </div>
+        <CaptainDetails />
       </div>
+
+      <div className="fixed w-full z-10 bottom-0  bg-white px-3 py-6 pt-12">
+    <RidePopUp />
+      </div>
+
     </div>
   )
 }
