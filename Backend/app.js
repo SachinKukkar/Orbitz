143650bd0f70c5c3e889ec600dc1e29441219544
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-01-22 23:48:18 +0530

    Google Maps API done - 7:54:04

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,8 @@ const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+
 connectToDb();
 
 app.use(cors());
@@ -22,5 +24,6 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps',mapsRoutes);
 
 module.exports = app;
